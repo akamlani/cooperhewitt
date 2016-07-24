1e2aa49466cea41eded69beef6b9c1368f7dec41
akamlani <akamlani@gmail.com>
2016-07-24 08:40:36 -0700

    Add initial data sources, code to extract collection metadata, and update README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# cooperhewitt
-Capstone Project for Galvanize Data Science Immersive Program
+# Cooper Hewitt
+For many years, approximately four years, the Cooper Hewitt design museum was closed while it
+underwent a transformation.  During this time they overhauled their technology to create
+an amazing experience for the visitor.  They wanted it to take the form of an engaging immersive
+experience, whereby visitors would interact with the technology.  During this time "The Pen" was
+born which allowed the museum to track which artworks (objects) were being interacted with via
+recorded timestamps and identifiers.  Recently this information has been made openly available to the public,
+in de-identified bundles to protect the privacy of the visitors.
+
+
+## Project Description
+Aggregating the Pen data and collection metadata (metadata about an object) can give further insights
+into how the museum is effectively using their assets and resources, notably "The Pen" and how exhibitions
+are planned and artworks chosen.  As the museum is heavily focused on improving the experience factor for
+the visitors, understanding the behavioral patterns and tastes would be a value-add to the them.
+
+
+## Cooper Hewitt Data Sources
+- [De-identified Pen Data](https://github.com/cooperhewitt/the-pen-data/)
+- [Collection Metadata](https://collection.cooperhewitt.org/api/methods/)
+
+## Configuration (API Keys)
+- Fill in the appropriate sections in config/api_cred.yml based on registration with Cooper Hewitt.
+
+## Data (Transformed)
+- collection_objects {.csv, .tsv, .pkl}
+    - Represents some of the metadata about a given artwork
+- exhibitions {.csv, .pkl}
+    - Represents when an exhibition took place
+- exhibition_objects {.json, .pkl}
+    - Represents which objects are associated with an exhibition (not directly associated w/pen data timestamps)
+- pen_collected_items {.csv, original}
+    - De-identified Tracked Pen data for a particular visitor
+
+## Metrics
+- [TBD]
+
+## Cooper Hewitt Resources
+- [Cooper Hewitt Labs](http://labs.cooperhewitt.org)
+- [Collection Stats](http://collection.cooperhewitt.org/stats)
+- Articles
+    - [Citizen Curation](http://tfmorris.blogspot.com/2012/10/citizen-curation-of-smithsonian-metadata.html)
+    - [Happy Birthday for the Pen](http://labs.cooperhewitt.org/2016/a-very-happy-open-birthday-for-the-pen/)
---
+++ src/ch_collections.py
@@ -0,0 +1,121 @@
+import yaml
+import requests
+import json
+import time
+import pandas as pd
+
+
+
+class Museum(object):
+    
+    def __init__(self):
+        self.url = self.create_url()
+        credentials = yaml.load(open('./../config/api_cred.yml'))
+        self.api_key = credentials['cooperhewitt_key']
+        self.access_token = credentials['cooperhewitt_token']
+    
+    def create_url(self):
+        hostname = 'api.collection.cooperhewitt.org'
+        endpoint = '/rest'
+        url = "https://" + hostname + endpoint + '/'
+        return url
+
+    def _execute(self, method, args):
+        # base method to execute a single request, limited to 500 records per page
+        now  = int(time.time())
+        args['timestamp'] = now
+        args['method'] = method
+        args['access_token'] = self.access_token
+        args['per_page'] = 500
+        response = requests.get(self.url, params=args)
+        if response.status_code == 200:
+            rsp_json = json.loads(response.text)
+            print "Page Response={0}, Num Records={1}, Pages={2}".format(response.status_code, rsp_json['total'], rsp_json['pages'])
+        else:
+            print "Page Response={0}".format(response.status_code)
+        return response
+
+    def _access_records(self, rsp_json, kf, format, state="initial"):
+        # handle the type of format conversion required
+        if format == "dataframe":
+            return pd.io.json.json_normalize(rsp_json)[kf][0]
+        elif format == "json":
+            if state == "initial": return rsp_json
+            else: return resp_json[kf][0]
+
+
+    def _execute_pages(self, method, args, kf, format="dataframe"):
+        # execute successive pages via pagination
+        response = self._execute(method, args)
+        if(response.status_code == 200):
+            rsp_json = json.loads(response.text)
+            records  = self._access_records(rsp_json, kf, format)
+            print "Initial Page Request: Response={0}, Num Records={1}, Args={2}" \
+                .format(response.status_code, rsp_json['total'], args['tag_id'] if 'tag_id' in args else {})
+        else:
+            print "Initial Page Request: Response={0}, Args={1}" \
+                .format(response.status_code, args['tag_id'] if 'tag_id' in args else {})
+            return pd.DataFrame()
+        
+        if( (rsp_json['stat'] == u'ok') and (int(rsp_json['pages']) > 1) ):
+            max_records  = int(rsp_json[u'total'])
+            max_pages    = int(rsp_json[u'pages'])
+            for page_id in range(2, max_pages+1):
+                args['page'] = page_id
+                response = self._execute(method, args)
+                rsp_json = json.loads(response.text)
+                if( (rsp_json['stat'] == u'ok') and (page_id <= int(rsp_json['pages'])) ):
+                    records += self._access_records(rsp_json, kf, format, state="pagination")
+            print "Page Results Found: {0}, Current Num Records: {1}".format(len(records) == max_records, len(records))
+        return (pd.DataFrame(records) if format == "dataframe" else records)
+
+
+
+#### begin specific site information about the museum
+    def site_information(self):
+        req_method = 'cooperhewitt.objects.locations.sites.getList'
+        rsp = self._execute(req_method, {})
+        rsp_json = json.loads(rsp.text)
+        if rsp_json['stat'] == 'ok':
+            metric_names = { 'id', 'name', 'count_objects', 'count_rooms', 'count_spots' }
+            metrics_json = { key:value for key,value in rsp_json['sites']['site'][0].items() if key in metric_names }
+            self.site_id = metrics_json['id']
+            return metrics_json
+        return rsp_json
+
+    def site_objects(self):
+        req_method = 'cooperhewitt.objects.locations.sites.getObjects'
+        rsp_json   = self.execute_pages(req_method, {'site_id': int(self.site_id)}, u'objects')
+        return rsp_json
+
+    def site_departments(self):
+        req_method = 'cooperhewitt.departments.getList'
+        rsp = self._execute(req_method, {})
+        rsp_json = json.loads(rsp.text)
+        self.df_depts = pd.DataFrame(rsp_json['departments'])
+        return self.df_depts
+
+    def site_exhibitions(self):
+        req_method = 'cooperhewitt.exhibitions.getList'
+        rsp = self._execute(req_method, {})
+        rsp_json = json.loads(rsp.text)
+        print "Response={0}, Num Records={1}".format(rsp.status_code, rsp_json['total'])
+        df_exhibitions = pd.DataFrame(rsp_json['exhibitions'])
+        return df_exhibitions
+
+    def site_objects_via_exhibition(self, exhibition_id, kw):
+        records = {}
+        req_method = 'cooperhewitt.exhibitions.getObjects'
+        rsp_json = self._execute_pages(req_method, {'exhibition_id': exhibition_id}, u'objects', format='json')
+        if rsp_json.get(kw):
+            records["_id"] = str(exhibition_id)
+            records[kw] = rsp_json[kw]
+        return records
+
+
+
+
+
+
+
+
