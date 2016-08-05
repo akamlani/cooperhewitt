5d1deb2145aff4904f0de2b7e29ed8869010cd5f
Ari Kamlani <akamlani@gmail.com>
2016-08-04 19:35:50 -0700

    add pen featurization matrix initial implementation

--- src/ch_pen.py
+++ src/ch_pen.py
@@ -3,6 +3,7 @@ import databases
 import os
 import utils
 
+
 class Pen(object):
     def __init__(self):
         export_path = "./../export/"
@@ -23,7 +24,6 @@ class Pen(object):
         df['created.dateformat.utc'] = df['created.dateformat'].dt.tz_localize('UTC')
         df['created.dateformat.est'] = df['created.dateformat.utc'].dt.tz_convert('US/Eastern')
         df['created.date.est']       = pd.to_datetime(df['created.dateformat.est'].dt.date)
-        
         # extract time period date information
         df['month']   = pd.DatetimeIndex(df['created.dateformat.est']).month
         df['year' ]   = pd.DatetimeIndex(df['created.dateformat.est']).year
@@ -33,10 +33,6 @@ class Pen(object):
         df['weekend'] = (pd.DatetimeIndex(df['created.dateformat.est']).dayofweek > 4).astype(int)
         df['hour' ]   = pd.DatetimeIndex(df['created.dateformat.est']).hour
         df['quarter'] = pd.DatetimeIndex(df['created.dateformat.est']).quarter
-        # feature engineeer dates
-        # map = {'morning',  'afternoon', 'evening'}
-        
-        
         # save the converted file to a csv
         df.to_csv("./../export/pen_transformed_raw.csv")
         df.to_pickle("./../export/pen_transformed_raw.pkl")
@@ -44,7 +40,6 @@ class Pen(object):
 
     def feature_engineer(self, df_exhibitions_in):
         self.df_pen = pd.read_pickle("./../export/pen_transformed_raw.pkl")
-
         # extract closing time events (evening or testings)
         self.df_pen['tagged_after_close'] = self.df_pen.apply(self._create_closing_time, axis=1)
         # set up tracking for exhibition events
@@ -55,13 +50,53 @@ class Pen(object):
         # save the converted file to a csv
         self.df_pen.to_csv("./../export/pen_transformed_features.csv")
         self.df_pen.to_pickle("./../export/pen_transformed_features.pkl")
+
+
+    def feature_dummies(self):
+        # create feature matrix to be used for modeling
+        frame = self.df_pen_meta_sub
+        df_hours = pd.cut(frame.hour, [0,11,15,18,24], labels=['morning', 'afternoon', 'evening', 'closed'])
+        df_hours_dum = pd.get_dummies(df_hours, drop_first=False)
+        df_quart_dum = pd.get_dummies(frame.quarter, prefix="quarter", drop_first=False)
+        # day of week
+        df_dow_dum   = pd.get_dummies(frame.dow, prefix="dow", drop_first=False)
+        df_dow_map   = {'dow_0':'Mon','dow_1':'Tues','dow_2':'Wed','dow_3':'Thur','dow_4':'Fri','dow_5':'Sat','dow_6':'Sun'}
+        df_dow_dum   = df_dow_dum.drop(['dow_0', 'dow_5', 'dow_6'], axis=1)
+        df_dow_dum.columns = map(lambda x: df_dow_map[x], df_dow_dum.columns)
+        # week of month
+        df_wom       = frame.day.map(lambda x: (x/7) + 1 if (x/7) < 4 else 4)
+        df_wom_dum   = pd.get_dummies(df_wom, prefix='wom', drop_first=False)
+        # year
+        df_year_dum  = pd.get_dummies(frame.year, prefix='year', drop_first=False)
+        # month
+        df_month_dum = pd.get_dummies(frame.month, prefix='month', drop_first=False)
+        # room ids, floors
+        df_rooms_dum = pd.get_dummies(frame['room.id'], prefix='room', drop_first=False)
+        df_floor_dum = pd.get_dummies(frame['room.floor'], prefix='floor', drop_first=False)
+        # room capacity (based on objects and spots)
+        # it looks like some rooms can only two room are capable of supporting a large num objects and therefore spots
+        # in this case for a given room => count_objects ~ count_spots
+        df_roomcap  = pd.cut(frame['room.count_objects'], [-10000, 500, 1000, 25000, 40000],
+                             labels=['room_small_cap', 'room_midsize_cap', 'room_large_cap', 'room_xlarge_cap'])
+        df_roomcap_dum = pd.get_dummies(df_roomcap, drop_first=False)
+        # spot turn-over (some spots change more frequently than others)
+        df_spot_freq = pd.cut(frame['spot.count_objects'], [-10000, 500, 1000, 1500, 2500],
+                              labels=['spot_constant_freq', 'spot_normal_freq', 'spot_high_freq', 'spot_dynamic_freq'])
+        df_spot_freq_dum = pd.get_dummies(df_spot_freq, drop_first=False)
+
+        # combine the dummies into a single frame
+        self.df_dummies   = pd.concat([df_hours_dum, df_dow_dum, df_wom_dum, df_year_dum, df_month_dum, df_quart_dum,
+                                       df_rooms_dum, df_floor_dum, df_roomcap_dum, df_spot_freq_dum], axis=1)
+        # create single feature matrix
+        cols = ['weekend', 'tagged_after_close', 'during_exhibition', 'visitor_drawn', 'meta_store', 'bursty']
+        self.df_features = pd.concat([self.df_dummies, frame[cols]], axis=1)
+
+
+    def custom_features(self, df_locations_in):
         # VISITOR DRAWN vs Wall objects
         self.df_visitordrawn = self.df_pen[self.df_pen['tool_id'] != 0]
         self.df_wallobjects  = self.df_pen[self.df_pen['tool_id'] == 0]
         self.df_pen['visitor_drawn'] = (self.df_pen['tool_id'] != 0).astype(int)
-
-
-    def custom_features(self, df_locations_in):
         # Tag Information (Temporal Locations)
         # is the visit_date during the same time as when the tag occurs
         self.df_pen = self.df_pen.apply(lambda el: self.tr.acquire_location_at_tag(el, df_locations_in), axis=1)
@@ -70,12 +105,16 @@ class Pen(object):
         self.create_bundle_daily_sequences()
         # Subset data to be utilized and imputate
         self.create_meta_tag()
+        # create feature matrix
+        self.feature_dummies()
 
+        # ADDITIONAL FEATURES to consider:
         # Weight given to a  bundle based on Frequent Visitor
         # Weight given to an object based if influencer
         # Models: Feature Importance based on if occured during exhibitions (as a label)
         # Likelihood Prediction of a tag being associated with an exhibition
-        
+        # AVG TAG/HR
+        # Time Spent at Museum
 
     def _create_closing_time(self, df_in):
         # Saturday corresponds to closing time and is open after hours
@@ -85,15 +124,14 @@ class Pen(object):
         ts_close = ts_close.tz_localize('US/Eastern')
         ts_after_hours = df_in["created.dateformat.est"] > ts_close
         return int(ts_after_hours)
-    
-    
+
     def _tag_exhibition_check(self, obj_id, exhibition_id, df_exhibitions_in):
         df_exhibit   = df_exhibitions_in[df_exhibitions_in['id']  == str(exhibition_id)]
         df_tag_match = self.df_pen[self.df_pen['refers_to_object_id'] == int(obj_id)]
         self.df_pen.ix[df_tag_match.index, "during_exhibition"] = \
         df_tag_match['created.date.est'].apply(lambda date: (date >= df_exhibit['created.date_start']) &
                                                (date <  df_exhibit['created.date_end']) ).values
-    
+
     def _tag_during_exhibition(self, exhibition_id, df_exhibitions_in):
         ds_objectids = self.db_query.query_records("exhibition", str(exhibition_id))
         if ds_objectids != None:
@@ -123,7 +161,7 @@ class Pen(object):
         self.df_pen_meta = self.df_pen.merge(bursty_bundle_rst, on=['bundle_id', 'created.date.est'], how='left')
         self.df_pen_meta[['bursty', 'mva', 'ntags']].fillna(-9999, inplace=True)
         bursty_bundle.sort_values(by='mva').mva.value_counts().sort_index()
-    
+
     def create_meta_tag(self):
         date_cols = ['day', 'dow', 'hour', 'month', 'quarter', 'weekend', 'year']
         event_cols = ['tagged_after_close', 'during_exhibition', 'visitor_drawn', 'meta_store', 'bursty', 'ntags', 'mva']
@@ -131,16 +169,19 @@ class Pen(object):
         cols = date_cols + event_cols + location_cols
         self.df_pen_meta_sub = self.tr.imputate(self.df_pen_meta[cols])
         self.df_pen_meta_sub.loc[:, location_cols] = self.df_pen_meta_sub[location_cols].astype(int)
-    
-    
+
+
     def create_object_sequences(self):
         # from a given daily sequence visit, what are the connections
         # for each tuple, extract a directed ordered sequence, removing the timestamp for now
         df_bundle_daily_seq_order = self.df_bundle_sequences.apply(lambda x: x[1])
 
 ### Metrics
-    def metrics(self):
+    def calc_metrics(self, df_objects_in):
         # metrics
+        self.metrics = {}
+        self.cp_metrics = {}
+        print "updating metrics"
         exhibitions = self.df_pen.during_exhibition.value_counts()
         station_metrics         = self.df_pen['tool_id'].value_counts()
         n_wallobjects_tagged    = station_metrics[0]
@@ -148,15 +189,11 @@ class Pen(object):
         n_wallobjects_unique    = len(self.df_pen.refers_to_object_id.unique())
         n_bundles_unique        = len(self.df_pen.bundle_id.unique())
         n_tag_obs               = self.df_pen.shape[0]
-        n_obj_meta_delta        = len( set(self.df_pen.refers_to_object_id.unique()) - set(df_locations_in.refers_to_object_id.unique()) )
-        n_obs_meta_delta        = self.df_pen[self.df_pen.refers_to_object_id.isin(df_locations_in.refers_to_object_id.unique())].shape[0]
-        
+        n_obj_meta_delta        = len( set(self.df_pen.refers_to_object_id.unique()) - set(df_objects_in.id.unique()) )
+        n_obs_meta_delta        = self.df_pen[self.df_pen.refers_to_object_id.isin(df_objects_in.id.unique())].shape[0]
+
         self.metrics = {'n_wall_objects_tagged': n_wallobjects_tagged, 'n_visitor_objects_tagged': n_visitorobjects_tagged}
         self.metrics.update({'n_unique_wallobjects': n_wallobjects_unique, 'n_unique_bundles': n_bundles_unique,
                             'n_tags': n_tag_obs, 'n_objects_meta_delta': n_obj_meta_delta, 'nobs_meta_delta': n_obs_meta_delta})
         self.metrics.update({'n_during_exhibition_tags': exhibitions[1], 'n_nonexhibition_tags': exhibitions[0]})
-
-
-
-
-
+        self.cp_metrics.update(self.metrics)
