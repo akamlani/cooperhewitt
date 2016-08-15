4e6b958dc4583dd8d89a2468f22c5f61432c9aa7
Ari Kamlani <akamlani@gmail.com>
2016-08-15 16:46:00 -0700

    updates to graphs and plots for iGraph processing and plotting

--- src/graphs.py
+++ src/graphs.py
@@ -1,16 +1,29 @@
-import networkx as nx
-from collections import defaultdict
 import operator
+import pandas as pd
+import cPickle as pickle
+from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import networkx as nx
+import igraph as ig
+
+
+class GraphData(object):
+    def __init__(self):
+        pass
 
 class GraphConn(object):
     def __init__(self):
         self.dag = defaultdict(list)
+        self.export_path = "./../export/"
+        self.df_sub_edges = pd.read_pickle(self.export_path  + "community_edges.pkl")
+        self.df_sub_nodes = pd.read_pickle(self.export_path  + "community_vertices.pkl")
+        #with open(export_path + 'igraph_data.pkl', 'rb') as f: self.gd = pickle.load(f)
 
-    def create_adj_basic(self, seq):
+    def networkx_build(self, seq):
+        # unoptimized implementation: trouble with scaling
         # its possible we have only visited a single object, there is nothing to do
         if not len(seq) > 1: return
         # else process all the nodes
@@ -23,16 +36,61 @@ class GraphConn(object):
         # we built a directed graph
         self.nx_dag = nx.DiGraph(self.dag, name='basic')
 
-    def display_network(self):
+    def networkx_display(self):
         # Layouts: Spring, Spectral, Random, Shell
         nx.draw(self.nx_dag, cmap = plt.get_cmap('jet'), edge_cmap=plt.cm.Blues,with_labels=False)
 
-    def linkanalysis(self):
+    def networkx_linkanalysis(self):
         # pagerank: ranking of the nodes in the graph based on the structure of the incoming links
         self.nx_dag_pr = nx.pagerank(self.nx_dag)
         self.nx_dag_prs = sorted(self.nx_dag_pr.items(), key=operator.itemgetter(1), reverse=True)
 
-    def metrics(self):
-        return {'num nodes': self.nx_dag.number_of_nodes(),
-                'num edges': self.nx_dag.number_of_edges(),
+    def networkx_metrics(self):
+        return {'num nodes':   self.nx_dag.number_of_nodes(),
+                'num edges':   self.nx_dag.number_of_edges(),
                 'num degrees': self.nx_dag.degree() }
+
+    def iGraph_build(self):
+        # build the graph nodes for iGraph (currently subsetted from Spark Graphx)
+        edges = self.df_sub_edges
+        src = edges.src.unique()
+        dst = edges.dst.unique()
+        vertices = list(set(src).union(set(dst)))
+        self.n_vertices = len(vertices)
+        # map the vertices as it requires index to be numeric
+        vertices_map = {val: idx for idx,val in enumerate(vertices) }
+        self.df_sub_nodes['id'] = pd.Series(vertices).apply(lambda x: int(vertices_map[x]))
+        # create a neighbor list
+        neighbors = list(map(tuple, edges.values))
+        neighbors_mapped = map(lambda p: tuple([vertices_map[pi] for pi in p]), neighbors)
+        self.G = ig.Graph(neighbors_mapped)
+
+        #communities = G.community_multilevel().membership
+        #n_communities = len(set(community))
+        #color_list = sns.color_palette("husl", n_communities)
+        #edge.betweenness.community(g, directed=F)
+        return neighbors_mapped
+
+    def iGraph_layout(self, neighbors):
+        layt=self.G.layout('grid_3d', dim=3)
+        # Retrieve X,Y,Z vertice coordinates
+        vertices = pd.DataFrame( [(layt[v][0], layt[v][1], layt[v][2]) for v in xrange(self.n_vertices)] )
+        Xn,Yn,Zn = vertices[0],vertices[1],vertices[2]
+        # Retrieve X,Y,Z edge coordinates
+        Xe=Ye=Ze = []
+        for (src,dst) in neighbors:
+            Xe+=[layt[src][0],layt[dst][0], None] # x-coordinates of edge ends
+            Ye+=[layt[src][1],layt[dst][1], None] # y-coordinates of edge ends
+            Ze+=[layt[src][2],layt[dst][2], None] # z-coordinates of edge ends
+        # Retrieve Labels
+        labels = list(self.df_sub_nodes.type)
+        group  = list(self.df_sub_nodes.group)
+        # set the Graph data
+        gx = GraphData()
+        gx.labels,gx.group = (labels, group)
+        gx.Xe,gx.Ye,gx.Ze = (Xe, Ye, Ze)
+        gx.Xn,gx.Yn,gx.Zn = (Xn, Yn, Zn)
+        gx.vertices = vertices
+        gx.layt = layt
+        with open(self.export_path + 'igraph_data.pkl', 'wb') as f: pickle.dump(gx, f)
+        return gx
--- src/plots.py
+++ src/plots.py
@@ -1,15 +1,20 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("darkgrid")
 sns.set_context("talk")
 sns.set_palette("husl")
 
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
 class Display(object):
     def __init__(self):
         pass
 
-    def create_subplots(self, params, (nrows, ncols, plt_size) ):
+    def create_subplots(self, params, (nrows, ncols, plt_size), filename='plots/pen_eda.png'):
         fig, _ = plt.subplots(nrows, ncols, figsize=plt_size)
 
         for idx, (ax, param_i) in enumerate(zip(fig.axes, params)):
@@ -37,11 +42,40 @@ class Display(object):
                 items = param_i.get('labels')
             ax.tick_params(axis='y', which='major', pad=15)
 
-
         #fig.delaxes(fig.axes[nrows*ncols-1])
         plt.subplots_adjust(bottom=0.05)
         plt.tight_layout()
-        fig.savefig('../plots/pen_eda.png', dpi=100)
+        fig.savefig(filename, dpi=100)
+
+    def plotly_graph(self, gx, filename):
+        axis=dict(showbackground=False, showline=False, zeroline=False, showgrid=False, showticklabels=False, title='')
+        trace1=go.Scatter3d(x=gx.Xe,y=gx.Ye,z=gx.Ze, mode='lines',
+                            line=go.Line(color='rgb(125,125,125)', width=1), hoverinfo='none')
+        trace2=go.Scatter3d(x=gx.Xn,y=gx.Yn,z=gx.Zn, mode='markers',name='artwork',
+                         marker=go.Marker(symbol='diamond',size=6,color=gx.group,colorscale='Viridis',
+                                       line=go.Line(color='rgb(50,50,50)', width=0.5)),
+                         text=gx.labels, hoverinfo='text',)
+
+        layout_grid = go.Layout(title="",
+                                width=1000, height=1000, showlegend=False,
+                                scene=go.Scene( xaxis=go.XAxis(axis),yaxis=go.YAxis(axis),zaxis=go.ZAxis(axis)),
+                                margin=go.Margin(t=35,b=0), hovermode='closest',
+                                paper_bgcolor='rgb(233,233,233)',
+                                annotations=go.Annotations([
+                                    go.Annotation(
+                                       showarrow=False,
+                                       text="",
+                                       xref='paper',yref='paper',x=0,y=0.01,xanchor='left',yanchor='bottom',
+                                       font=go.Font(size=14) )
+                                ]), )
+
+        data=go.Data([trace1, trace2])
+        fig=go.Figure(data=data, layout=layout_grid)
+        py.iplot(fig, filename=filename)
+        py.image.save_as(fig, filename=filename)
+        return fig
+
+
 
 
 
