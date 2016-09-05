08fb685f8c5c397bf5be725ece1284cb5da9ec8f
Ari Kamlani <akamlani@gmail.com>
2016-09-04 23:31:42 -0700

    update aws connection to copy down files from s3

--- src/aws.py
+++ src/aws.py
@@ -4,43 +4,35 @@ import boto
 
 class AwsConn(object):
     def __init__(self):
-        self.export_path = './../export/'
+        root_path = os.environ['COOPERHEWITT_ROOT']
+        self.export_path = root_path + "/export/"
         self.access_key = os.environ['AWS_ACCESS_KEY_ID']
         self.secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
         self.conn = boto.connect_s3(self.access_key, self.secret_access_key)
 
-    def acquire_files(self, path='projects/cooper-hewitt/data/'):
+    def acquire_files(self, path_name):
         bucket_names = [b.name for b in self.conn.get_all_buckets()]
         bucket = self.conn.get_bucket(bucket_names[1])
         filenames = [f.name for f in bucket.list()]
-        filenames = [f for f in filenames if 'projects/cooper-hewitt/data/' in f]
+        filenames = [f for f in filenames if path_name in f and f != path_name]
         return filenames
 
-    def create_resource(self, resource_file,
-                        bucket_name='akamlani-galvanize',
-                        data_path='projects/cooper-hewitt/data/'):
+    def create_resource(self, bucket_name, path_name, resource_file):
         export_path = self.export_path + resource_file
         bucket = self.conn.get_bucket(bucket_name)
-        key = bucket.get_key(data_path + resource_file)
+        key = bucket.get_key(path_name + resource_file)
+        # assuming we have enough space, else create symbolic link beforehand
         if not os.path.exists(self.export_path):
-            os.mkdir("./../export")
+            os.mkdir(self.export_path)
         if not os.path.isfile(export_path):
-	    print export_path, data_path + resource_file,  key
-	    with open(export_path, 'wb') as f: key.get_contents_to_file(f)
-        return export_path
+            print export_path, path_name + resource_file,  key
+            with open(export_path, 'wb') as f: key.get_contents_to_file(f)
 
 
-
-# code for AWS Connection buckets
-aws = AwsConn()
-res = {"collection_objects": ('collection_objects.pkl', 'pkl'),
-       "exhibition_objects": ('exhibition_objects.pkl', 'pkl'),
-       "exhibitions":        ('exhibitions.pkl', 'pkl'),
-       "pen":                ('pen_collected_items.csv', 'csv'),
-       "pen_tr_raw":         ('pen_transformed_raw.pkl', 'pkl'),
-       "pen_tr_features":    ('pen_transformed_features.pkl', 'pkl'),
-       "temporal_loc":       ('temporal_locations.pkl', 'pkl'),
-       "temporal_exh":       ('temporal_exhibitions.pkl', 'pkl'),
-       "spark_meta":         ('penmeta_spark.pkl', 'pkl')}
-
-file_res = {k: aws.create_resource(file) for k, (file, file_type) in res.iteritems() }
+if __name__ == '__main__':
+    path_name    = 'projects/cooper-hewitt/data/'
+    bucket_name  = 'akamlani-galvanize'
+    # code for AWS Connection buckets
+    conn = AwsConn()
+    filenames = [filename.replace(path_name, '') for filename in conn.acquire_files(path_name)]
+    [conn.create_resource(bucket_name, path_name, filename) for filename in filenames]
